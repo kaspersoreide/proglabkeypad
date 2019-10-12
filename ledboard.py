d3bdbacbbfdb3b65bf8a867825c395b20805e913
kaspersoreide <kasper.soreide@gmail.com>
2019-10-12 13:24:07 +0100

    leds work!

--- ledboard.py
+++ ledboard.py
@@ -5,14 +5,15 @@ GPIO.setmode(GPIO.BCM)
 
 class LEDboard:
     def __init__(self):
-        self.LEDs = [16, 20, 21]
+        self.LEDs = [16, 21, 20]
         self.leds_enabled = [False, False, False,
                              False, False, False]
         for i in range(3):
             GPIO.setup(self.LEDs[i], GPIO.OUT)
     def turn_off_LEDs(self):
         for i in range(3):
-            GPIO.output(self.LEDs[i], GPIO.LOW)
+            GPIO.setup(self.LEDs[i], GPIO.IN)
+            #GPIO.output(self.LEDs[i], GPIO.LOW)
     def flash_LEDs(self):
         self.turn_off_LEDs()
         for i in range(6):
@@ -22,11 +23,19 @@ class LEDboard:
             switch = i % 2
             pin1 = self.LEDs[led_index]
             pin2 = self.LEDs[(led_index + 1) % 3] 
+            pin3 = self.LEDs[(led_index + 2) % 3] 
             self.turn_off_LEDs()
+            GPIO.setup(pin1, GPIO.OUT)
+            GPIO.setup(pin2, GPIO.OUT)
+            GPIO.setup(pin3, GPIO.IN)
             if switch == 1:
+                GPIO.output(pin1, GPIO.LOW)
                 GPIO.output(pin2, GPIO.HIGH)
             else:
+                GPIO.output(pin2, GPIO.LOW)
                 GPIO.output(pin1, GPIO.HIGH)
+        self.turn_off_LEDs()
+
     def enable_LEDs(self):
         for i in range(6):
             self.leds_enabled[i] = True
@@ -39,6 +48,24 @@ class LEDboard:
         t = time.time()
         while time.time() - t < duration:
             self.flash_LEDs()
+        self.disable_LEDs()
+
+    def test_leds(self):
+        pin1 = 21
+        pin2 = 16
+        pin3 = 20
+        GPIO.setup(pin1, GPIO.IN)
+        GPIO.setup(pin2, GPIO.OUT)
+        GPIO.setup(pin3, GPIO.OUT)
+        GPIO.output(pin3, GPIO.HIGH)
+        for i in range(6):
+            led_index = i // 2
+            switch = i % 2
+            pin1 = self.LEDs[led_index]
+            pin2 = self.LEDs[(led_index + 1) % 3] 
+            pin3 = self.LEDs[(led_index + 2) % 3] 
+            print(str(led_index), str(switch), str(pin1), str(pin2), str(pin3))
+
 
     def power_up(self):
         self.disable_LEDs()
@@ -49,13 +76,37 @@ class LEDboard:
                 self.flash_LEDs()
         self.disable_LEDs()
 
+    def power_down(self):
+        self.enable_LEDs()
+        for i in range(6):
+            t = time.time()
+            while time.time() - t < 0.7:
+                self.flash_LEDs()
+            self.leds_enabled[i] = False 
+        self.disable_LEDs()
+
     def wrong(self):
         self.disable_LEDs()
         for i in range(3):
             t = time.time()
-            for i in range(6):
-                self.leds_enabled[i] = not self.leds_enabled[i]
+            for j in range(6):
+                self.leds_enabled[j] = not self.leds_enabled[j]
             while time.time() - t < 0.4:
                 self.flash_LEDs()
         self.disable_LEDs()
+
+    def correct(self):
+        self.disable_LEDs()
+        for i in range(3):
+            t = time.time()
+            for j in range(3):
+                if i % 2 == 1:
+                    self.leds_enabled[2 * j + 1] = True
+                    self.leds_enabled[2 * j] = False
+                else:    
+                    self.leds_enabled[2 * j + 1] = False
+                    self.leds_enabled[2 * j] = True
+            while time.time() - t < 0.9:
+                self.flash_LEDs()
+
 
